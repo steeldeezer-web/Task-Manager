5e88417d8acb331dd3af440fc10bc71916fb0a23
steeldeezer-web <steel.deezer@gmail.com>
2026-04-08 23:18:01 +0500

    решен вопрос бутылочного горлышка в методе createTask() в классе TaskServise

--- java/ru/steeldeezer/service/TaskService.java
+++ java/ru/steeldeezer/service/TaskService.java
@@ -1,22 +1,26 @@
 package ru.steeldeezer.service;
+import ru.steeldeezer.exception.UserNotFoundException;
 import ru.steeldeezer.model.*;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TaskService {
-    private static Map<UUID, User> userMap = new HashMap<>();
-    private static Map<UUID, Task> taskMap = new HashMap<>();
+    private static Map<UUID, User> userMap = new ConcurrentHashMap<>();
+    private static Map<UUID, Task> taskMap = new ConcurrentHashMap<>();
     // МЕТОД 1: Регистрация юзера
     public User createUser(String name){
         User user = new User(name);
         userMap.put(user.getUuid(), user);
         return user;
     }
+    //потокобезопасное создание таска
     public Task createTask(UUID userId,String title, String description){
-        if(!userMap.containsKey(userId)){
-            throw new RuntimeException("Пользователь не найден");
-        }
-        User user = userMap.get(userId);
-        Task task = new Task(user, title, description);
-        taskMap.put(task.getId(), task);
-        return task;
+        return taskMap.compute(UUID.randomUUID(),(id, existingTask) ->{
+            User user = userMap.get(userId);
+            if(user == null) throw  new UserNotFoundException("Пользователь не найден");
+            return new Task(user, title, description);
+        });
     }
 }
