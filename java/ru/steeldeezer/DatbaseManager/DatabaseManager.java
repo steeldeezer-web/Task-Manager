0eb3a7509ed59cf5cbd1e23b2b31a72d241b3a73
steeldeezer-web <steel.deezer@gmail.com>
2026-04-09 10:37:34 +0500

    Добавлен класс и пакет ru.steeldeezer.DatabaseManager

---
+++ java/ru/steeldeezer/DatbaseManager/DatabaseManager.java
@@ -0,0 +1,15 @@
+package ru.steeldeezer.DatbaseManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String URL = "jdbc:posgresql://localhost:5432/postgres";
+    public static final String USER = "postgres";
+    public static final String PASS = "SuBJqUXU1271";
+
+    public static Connection getConnection() throws SQLException{
+      return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
